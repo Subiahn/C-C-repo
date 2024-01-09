669a10ea8e4023066fc8055286dc680a5c48b1b5
Subiahn <130112343+Subiahn@users.noreply.github.com>
2024-01-09 22:19:25 +0900

    plus-minus
    사용자로 부터 두 정수를 입력 받아 뺄셈을 수행하고 결과를 출력하는 프로그램을 작성. 단, 절대로 뺄셈 연산자를 사용하지 말고 비트 연산자를 이용해 구현.

---
+++ plus-minus.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(void) {
+	int a = 0, b = 0;
+	int result = 0;
+	scanf_s("%d%d", &a, &b);
+	//result = a - b;
+	//result = a - b;
+	result = a + (~b + 1); // 2 의 보수 ;
+	printf("%d\n", result);
+	return 0;
+}
