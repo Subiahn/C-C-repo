42b12c3778462fec6f524b19f1ba5cc001b2213b
Subiahn <130112343+Subiahn@users.noreply.github.com>
2024-01-26 01:02:23 +0900

    Create array_max.c
    for문을 사용해 int[5] 배열에 저장된 값 중 가장 작은 값을 찾아 출력 하는 프로그램을 작성. int[0] 요소에 최솟값이 저장될 수 있도록 개발. 각 배열 요소를 비교하는 과정에서 두 배열 요소의 값은 덮어 써서 사라지지 않도록 교환

---
+++ array_max.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void) {
+
+	int arr1[5] = {30, 40, 10, 50, 20};
+
+	for (int i = 0; i < 5; ++i)
+	{
+		if (arr1[0] > arr1[i])
+		{
+			int tmp = arr1[0];
+			arr1[0] = arr1[i];
+			arr1[i] = tmp;
+		}
+	}
+
+	for(int i = 0; i < 5; ++i)
+
+		printf("%d\t", arr1[i]);
+	putchar('\n');
+	return 0;
+}
