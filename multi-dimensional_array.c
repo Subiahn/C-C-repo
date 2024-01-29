df9f134f18699ac8756521142b1c82480b98d184
Subiahn <130112343+Subiahn@users.noreply.github.com>
2024-01-29 22:13:26 +0900

    Create multi-dimensional_array.c
    다차원 배열 1

---
+++ multi-dimensional_array.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+	int aList[5][5] = {0};
+	for (int i = 0; i < 5; ++i)
+	{
+		for (int j = 0; j < 5; ++j)
+			printf("%d\t", aList[i][j]);
+		putchar('\n');
+	}
+	return 0;
+}
